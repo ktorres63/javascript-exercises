1b03e7feff258a647b1daba6cc6dd3d5c2b6ec4b
ktorres63 <ktorresaro@unsa.edu.pe>
2024-03-30 12:13:19 -0500

    Use of arguments object

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function (...args) {
+  const array = arguments[0]
+  for(let i = 1; i < arguments.length; i++){
+    array.splice(array.indexOf(arguments[i]),1)
+  }
 
+
+  // array.splice(array.indexOf(elem), 1);
+  return array;
 };
 
 // Do not edit below this line
