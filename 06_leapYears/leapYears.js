88cd36f7628fc5d2174c2f19ffbc17648a85f8dd
ktorres63 <ktorresaro@unsa.edu.pe>
2024-03-30 23:12:10 -0500

    Finish leapYear exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  let resp = false;
+  if((year % 4 == 0 & year % 100 != 0) || (year %400 == 0)){
+    return true
+  }
+  return resp
 };
 
 // Do not edit below this line
