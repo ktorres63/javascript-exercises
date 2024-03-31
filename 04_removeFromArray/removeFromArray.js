7af28c359216a3815b87c0c0dd4669f542959305
ktorres63 <ktorresaro@unsa.edu.pe>
2024-03-30 21:28:54 -0500

    Finish 04 javascript exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,18 +1,20 @@
 const removeFromArray = function (...args) {
-  const array = arguments[0];
+  let array = arguments[0];
   const elem = arguments[1];
-  
- 
-  return deleteElem(array,elem);
-};
-function deleteElem(array,elem){
-  let resp = []
-  for(let i = 0; i<array.length;i++){
-      if(array[i] !== elem){
-          resp.push(array[i])
-      }
+
+  for (let i = 1; i < arguments.length; i++) {
+    array = deleteElem(array, arguments[i]);
   }
-  return resp
+  return array;
+};
+function deleteElem(array, elem) {
+  let resp = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] !== elem) {
+      resp.push(array[i]);
+    }
+  }
+  return resp;
 }
 // Do not edit below this line
 module.exports = removeFromArray;
