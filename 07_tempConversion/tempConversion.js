3770045a4fefab822d7e1bd315e847175579d563
ktorres63 <ktorresaro@unsa.edu.pe>
2024-03-31 18:23:45 -0500

    Finish 07 javascript exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,23 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fah) {
+  rsp = (fah-32)/1.8
+  rsp = toInteger(rsp)
+  return rsp ;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celc) {
+  rsp = (celc * 1.8)+32
+  rsp = toInteger(rsp)
+  return rsp ;
+  
 };
+
+function toInteger(num){
+  num = parseFloat(num.toFixed(1));
+  if((num * 10)%10 === 0){
+    num= parseInt(rsp)
+  }
+  return num ;
+}
 
 // Do not edit below this line
 module.exports = {
