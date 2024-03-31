b4b9024c07f901ad89927f3a3a342ac5e012b212
ktorres63 <ktorresaro@unsa.edu.pe>
2024-03-30 21:21:01 -0500

    Add function to delete many coincidences

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,13 +1,18 @@
 const removeFromArray = function (...args) {
-  const array = arguments[0]
-  for(let i = 1; i < arguments.length; i++){
-    array.splice(array.indexOf(arguments[i]),1)
-  }
-
-
-  // array.splice(array.indexOf(elem), 1);
-  return array;
+  const array = arguments[0];
+  const elem = arguments[1];
+  
+ 
+  return deleteElem(array,elem);
 };
-
+function deleteElem(array,elem){
+  let resp = []
+  for(let i = 0; i<array.length;i++){
+      if(array[i] !== elem){
+          resp.push(array[i])
+      }
+  }
+  return resp
+}
 // Do not edit below this line
 module.exports = removeFromArray;
