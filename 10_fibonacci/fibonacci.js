0408e366641fc1c3434d171cafb80ba89b361f6b
ktorres63 <ktorresaro@unsa.edu.pe>
2024-04-24 16:50:12 -0500

    Finish exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function (str) {
+  let num = parseInt(str);
+  if (num < 0) {
+    return "OOPS";
+  } else if (num === 0) {
+    return 0;
+  } else {
+    let fib = [1, 1];
+    for (let i = 2; i < num + 2; i++) {
+      fib.push(fib[i - 2] + fib[i - 1]);
+    }
+    return fib[num - 1]; //[1, 1, 2, 3, 5, 8]
+  }
 };
 
 // Do not edit below this line
