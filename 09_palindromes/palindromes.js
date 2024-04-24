1f280ffa5646852be1790a3074e9cdedf7f19a1e
ktorres63 <ktorresaro@unsa.edu.pe>
2024-04-24 15:50:45 -0500

    Solve palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,28 @@
-const palindromes = function () {
+function getCleanWord(str) {
+  let cleanWord = "";
+  for (e of str.toLowerCase()) {
+    if (e.charCodeAt(0) >= 97 && e.charCodeAt(0) <= 122 || e.charCodeAt(0)>= 48 && e.charCodeAt(0)<=57) {
+      cleanWord += e;
+    }
+  }
+  return cleanWord.split("");
+}
 
+const palindromes = function (str) {
+  let word = getCleanWord(str);
+
+  let size = word.length;
+  let half = Math.floor(size / 2); //3
+  let partA = word.slice(0, half).join("");
+  let partB = word
+  .slice(half + 1)
+  .reverse()
+  .join("");
+
+  if (size % 2 == 0) 
+    partB = word.slice(half).reverse().join("");
+ 
+  return partA === partB;
 };
 
 // Do not edit below this line
